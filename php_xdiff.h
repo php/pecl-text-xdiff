eeb6c6c877f3a17dd2418c172d4af25742d1f89c
Rasmus Lerdorf <rasmus@lerdorf.com>
2021-01-14 15:23:43 -0800

    Don't need these here

--- php_xdiff.h
+++ php_xdiff.h
@@ -39,22 +39,6 @@ extern zend_module_entry xdiff_module_entry;
 PHP_MINIT_FUNCTION(xdiff);
 PHP_MINFO_FUNCTION(xdiff);
 
-PHP_FUNCTION(xdiff_file_diff);
-PHP_FUNCTION(xdiff_file_bdiff);
-PHP_FUNCTION(xdiff_file_patch);
-PHP_FUNCTION(xdiff_file_bpatch);
-PHP_FUNCTION(xdiff_file_merge3);
-PHP_FUNCTION(xdiff_file_rabdiff);
-PHP_FUNCTION(xdiff_file_bdiff_size);
-
-PHP_FUNCTION(xdiff_string_diff);
-PHP_FUNCTION(xdiff_string_bdiff);
-PHP_FUNCTION(xdiff_string_patch);
-PHP_FUNCTION(xdiff_string_bpatch);
-PHP_FUNCTION(xdiff_string_merge3);
-PHP_FUNCTION(xdiff_string_rabdiff);
-PHP_FUNCTION(xdiff_string_bdiff_size);
-
 #endif	/* PHP_XDIFF_H */
 
 
